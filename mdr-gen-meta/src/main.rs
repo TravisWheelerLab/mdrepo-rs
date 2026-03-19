850f89bcac662cf06b9161436006c44d9afcf1f0
Ken Youens-Clark <kyclark@gmail.com>
2026-03-19 16:31:19 -0700

    now generating full Meta

--- mdr-gen-meta/src/main.rs
+++ mdr-gen-meta/src/main.rs
@@ -1,12 +1,17 @@
 use anyhow::{bail, Result};
 use clap::Parser;
 use dotenvy::dotenv;
-//use libmdrepo::metadata::Meta;
+use libmdrepo::{constants, metadata};
 use mdr_db::ops;
-use mdr_gen_meta::types::{Args, Server};
-use std::env;
+use mdr_gen_meta::types::{Args, FileFormat, Server};
+use std::{
+    env,
+    fs::File,
+    io::{self, Write},
+    path::Path,
+};
 
-//const DEFAULT_ORCID: &str = "0000-0000-0000-0000";
+const DEFAULT_ORCID: &str = "0000-0000-0000-0000";
 
 // --------------------------------------------------
 fn main() {
@@ -34,6 +39,33 @@ fn run(args: Args) -> Result<()> {
     };
     let mut conn = mdr_db::connect(&url);
     let sim = ops::get_simulation(&mut conn, args.simulation_id)?;
+    let software = ops::get_software(&mut conn, sim.software_id.unwrap())?;
+    let pdb = sim
+        .pdb_id
+        .map(|pdb_pk| ops::get_pdb(&mut conn, pdb_pk))
+        .transpose()?;
+    let (_, sim2uniprot) = ops::list_simulation_uniprots(
+        &mut conn,
+        Some(args.simulation_id),
+        None,
+        None,
+        None,
+    )?;
+    let mut uniprot_ids: Vec<String> = vec![];
+    for s2u in sim2uniprot {
+        if let Ok(uniprot) = ops::get_uniprot(&mut conn, s2u.uniprot_id) {
+            uniprot_ids.push(uniprot.uniprot_id.to_string())
+        }
+    }
+
+    let water = match (&sim.water_type, &sim.water_density) {
+        (Some(model), Some(density_kg_m3)) => Some(metadata::Water {
+            model: model.clone(),
+            density_kg_m3: density_kg_m3.clone(),
+        }),
+        _ => None,
+    };
+
     let lead_contributor_orcid = match sim.created_by_id {
         Some(user_id) => {
             let (_count, res) = ops::list_social_accounts(
@@ -43,56 +75,226 @@ fn run(args: Args) -> Result<()> {
                 None,
                 None,
             )?;
-            dbg!(&res);
             res.first().map(|v| v.uid.to_string())
         }
         _ => None,
     };
-    dbg!(&lead_contributor_orcid);
 
-    //dbg!(&sim);
-    //let lead_contributor_orcid =
-    //    sim.created_by_id.map(|id| ops::get_user(&mut conn, id));
-    //dbg!(&lead_contributor_orcid);
+    let (_, up_files) = ops::list_uploaded_files(
+        &mut conn,
+        None,
+        Some(args.simulation_id),
+        None,
+        None,
+    )?;
 
-    //let up_files = ops::list_uploaded_files(
-    //    &mut conn,
-    //    None,
-    //    Some(args.simulation_id),
-    //    None,
-    //    None,
-    //)?;
-    //dbg!(&up_files);
+    let mut additional_files: Vec<metadata::AdditionalFile> = vec![];
+    let mut trajectory_file_name: Option<String> = None;
+    let mut structure_file_name: Option<String> = None;
+    let mut topology_file_name: Option<String> = None;
+    for up_file in up_files.iter().filter(|f| f.is_primary) {
+        match up_file.file_type.as_str() {
+            "Trajectory" => trajectory_file_name = Some(up_file.filename.clone()),
+            "Structure" => structure_file_name = Some(up_file.filename.clone()),
+            "Topology" => topology_file_name = Some(up_file.filename.clone()),
+            _ => additional_files.push(metadata::AdditionalFile {
+                file_name: up_file.filename.clone(),
+                file_type: up_file.file_type.clone(),
+                description: up_file.description.clone(),
+            }),
+        }
+    }
 
-    //let meta = Meta {
-    //    lead_contributor_orcid: sim.,
-    //    trajectory_file_name: "",
-    //    structure_file_name: "",
-    //    topology_file_name: "",
-    //    temperature_kelvin: "",
-    //    integration_timestep_fs: "",
-    //    short_description: "",
-    //    software_name: "",
-    //    software_version: "",
-    //    mdrepo_id: "",
-    //    description: "",
-    //    toml_version: "",
-    //    user_accession: "",
-    //    run_commands: "",
-    //    forcefield: "",
-    //    forcefield_comments: "",
-    //    protonation_method: "",
-    //    replicate_id: "",
-    //    pdb_id: "",
-    //    uniprot_ids: "",
-    //    water: "",
-    //    additional_files: "",
-    //    ligands: "",
-    //    solvents: "",
-    //    dois: "",
-    //    papers: "",
-    //    external_links: "",
-    //    contributors: "",
-    //};
+    if ![
+        &trajectory_file_name,
+        &structure_file_name,
+        &topology_file_name,
+    ]
+    .iter()
+    .all(|val| val.is_some())
+    {
+        bail!("Missing one of required files!")
+    }
+
+    let (_, ligands_res) =
+        ops::list_ligands(&mut conn, None, Some(args.simulation_id), None, None)?;
+
+    let ligands = if ligands_res.is_empty() {
+        None
+    } else {
+        Some(
+            ligands_res
+                .iter()
+                .map(|val| metadata::Ligand {
+                    name: val.name.clone(),
+                    smiles: val.smiles_string.clone(),
+                })
+                .collect::<Vec<_>>(),
+        )
+    };
+
+    let (_, solvents_res) =
+        ops::list_solvents(&mut conn, None, Some(args.simulation_id), None, None)?;
+
+    let solvents = if solvents_res.is_empty() {
+        None
+    } else {
+        Some(
+            solvents_res
+                .iter()
+                .map(|val| metadata::Solvent {
+                    name: val.name.clone(),
+                    concentration_mol_liter: val.concentration.clone(),
+                })
+                .collect::<Vec<_>>(),
+        )
+    };
+
+    let (_, links_res) = ops::list_external_links(
+        &mut conn,
+        None,
+        Some(args.simulation_id),
+        None,
+        None,
+    )?;
+
+    let external_links = if links_res.is_empty() {
+        None
+    } else {
+        Some(
+            links_res
+                .iter()
+                .map(|val| metadata::ExternalLink {
+                    url: val.url.clone(),
+                    label: val.label.clone(),
+                })
+                .collect::<Vec<_>>(),
+        )
+    };
+
+    let (_, contributors_res) =
+        ops::list_contributions(&mut conn, None, Some(args.simulation_id), None, None)?;
+
+    let contributors = if contributors_res.is_empty() {
+        None
+    } else {
+        Some(
+            contributors_res
+                .iter()
+                .map(|val| metadata::Contributor {
+                    name: val.name.clone().unwrap(),
+                    email: val.email.clone(),
+                    institution: val.institution.clone(),
+                    orcid: val.orcid.clone(),
+                })
+                .collect::<Vec<_>>(),
+        )
+    };
+
+    let (_, sim2pub) = ops::list_simulation_pubs(
+        &mut conn,
+        Some(args.simulation_id),
+        None,
+        None,
+        None,
+    )?;
+    let mut papers: Vec<metadata::Paper> = vec![];
+    for s2p in sim2pub {
+        if let Ok(val) = ops::get_pub(&mut conn, s2p.pub_id) {
+            papers.push(metadata::Paper {
+                title: val.title.clone(),
+                authors: val.authors.clone(),
+                journal: val.journal.clone(),
+                volume: val.volume.clone() as u32,
+                number: val.number.clone(),
+                year: val.year.clone() as u32,
+                pages: val.pages.clone(),
+                doi: Some(val.doi),
+            });
+        }
+    }
+
+    let meta = metadata::Meta {
+        lead_contributor_orcid: lead_contributor_orcid
+            .unwrap_or(DEFAULT_ORCID.to_string()),
+        trajectory_file_name: trajectory_file_name.unwrap(),
+        structure_file_name: structure_file_name.unwrap(),
+        topology_file_name: topology_file_name.unwrap(),
+        temperature_kelvin: sim.temperature.unwrap() as u32,
+        integration_timestep_fs: sim.integration_timestep_fs.unwrap() as u32,
+        short_description: sim.short_description.unwrap(),
+        software_name: software.name,
+        software_version: software.version.unwrap_or("".to_string()),
+        mdrepo_id: Some(format!("MDR{:08}", args.simulation_id)),
+        description: sim.description,
+        toml_version: Some(constants::METADATA_TOML_VERSION),
+        user_accession: sim.user_accession,
+        run_commands: sim.run_commands,
+        forcefield: sim.forcefield,
+        forcefield_comments: sim.forcefield_comments,
+        protonation_method: sim.protonation_method,
+        replicate_id: None, // TODO fix?
+        pdb_id: pdb.map(|val| val.pdb_id),
+        uniprot_ids: if uniprot_ids.is_empty() {
+            None
+        } else {
+            Some(uniprot_ids)
+        },
+        water,
+        additional_files: if additional_files.is_empty() {
+            None
+        } else {
+            Some(additional_files)
+        },
+        ligands,
+        solvents,
+        dois: None,
+        papers: if papers.is_empty() {
+            None
+        } else {
+            Some(papers)
+        },
+        external_links,
+        contributors,
+    };
+
+    let mut out_file = open_outfile(&args.outfile)?;
+    let format = args.format.clone().unwrap_or(guess_format(&args.outfile));
+    write!(
+        out_file,
+        "{}",
+        if format == FileFormat::Json {
+            meta.to_json()?
+        } else {
+            meta.to_toml()?
+        }
+    )?;
+
     Ok(())
+}
+
+// --------------------------------------------------
+fn open_outfile(filename: &str) -> Result<Box<dyn Write>> {
+    match filename {
+        "-" => Ok(Box::new(io::stdout())),
+        out_name => Ok(Box::new(File::create(out_name)?)),
+    }
+}
+
+// --------------------------------------------------
+fn guess_format(filename: &str) -> FileFormat {
+    if filename == "-" {
+        FileFormat::Toml
+    } else {
+        match Path::new(filename).extension() {
+            Some(ext) => {
+                if ext == "json" {
+                    FileFormat::Json
+                } else {
+                    FileFormat::Toml
+                }
+            }
+            _ => FileFormat::Toml,
+        }
+    }
 }
